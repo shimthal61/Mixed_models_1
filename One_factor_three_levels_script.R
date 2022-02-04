e7608f940044999c236c2d01c930a635c3d5e9c3
Harvey Schneider <50023408+shimthal61@users.noreply.github.com>
2022-02-04 11:30:15 +0000

    Built our visualisation

--- One_factor_three_levels_script.R
+++ One_factor_three_levels_script.R
@@ -7,3 +7,14 @@ mixed_model_gaze <- mixed_model_gaze %>%
          Subject = factor(Subject),
          Item = factor(Item))
 
+(mixed_model_gaze_plot <- mixed_model_gaze %>% 
+  ggplot(aes(x = Condition, y = Gaze, colour = Condition)) +
+  geom_violin(width = 0.5) +
+  geom_point(alpha = 0.2, position = position_jitter(width = 0.1, seed = 42)) +
+  stat_summary(fun.data = 'mean_cl_boot', colour = 'black') +
+  guides(colour = 'none') +
+  theme_minimal() +
+  labs(x = "Gaze Duration (ms.)",
+       y = "Condition",
+       title = "The effect of Item Valence on Gaze Duration") +
+  coord_flip())
